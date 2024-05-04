98f4ad5f9a046e27bf42c24cc684323f859b0757
Ensky65 <wilensky.coleau@stonybrook.edu>
2024-05-04 19:58:38 -0400

    HospitalData.h

--- HospitalData.h
+++ HospitalData.h
@@ -5,6 +5,51 @@ using namespace std;
 
 class HospitalData 
 {
-    //meta data
+    class Substation
+    {
+        class Team
+            {
+                
+                class Surgery
+                {
+                    //Making it a node
+                    Surgery *next;
+                    Surgery *prev;
+                    //metadata
+                    string name;
+                    string date;
+                    string time;
+                    string room;
+                    string surgeon;
+                    string anesthesiologist;
+                    string nurse;
+                    string patient;
+                    string status;
+                };
+                public: 
+                int avergaeTime;
+                int averageDifiiulty;
+                string name; 
+                //ensure that each team is a doublylinked list of surgeries
+                private: 
+                Surgery *head; 
+                Surgery *tail;
+                public: 
+                Team *next; 
+                Team *prev;
+            };
+            private: 
+            Team *head; 
+            Team *tail; 
+            public: 
+            string bestTeam; 
+            string worstTeam;
+            
+    };
+    Substation EB; 
+    Substation RL; 
+    Substation OF;
+    Substation TM; 
+    Substation HD; 
     
 };
