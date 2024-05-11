9336e14b27d0d4933fb7ccf310113ba96e32d9b8
FuzzyPenguin <scokro7141@gmail.com>
2024-05-10 22:24:37 -0400

    bomb rushing the last bits of question 5

--- natural-disasters.cpp
+++ natural-disasters.cpp
@@ -1,5 +1,7 @@
 #include "naturaldisasters.h"
-#include <cstddef>
+#include <iostream>
+#include <math.h>
+using namespace std;
 
 Outage::Outage(const int& y, const int& t, const int& m, const int& i)
 {
@@ -13,6 +15,8 @@ Outage::Outage(const int& y, const int& t, const int& m, const int& i)
 Outages::Outages()
 {
     tail = NULL;
+    total_hours_out = 0;
+    total_customers_hours_interrupted = 0;
 };
 
 void Outages::add_outage(Outage* new_outage)
@@ -21,4 +25,34 @@ void Outages::add_outage(Outage* new_outage)
         tail->next = new_outage;
     }
     tail = new_outage;
+}
+
+double Outages::predict_next_year_performance(const int& surgery_points, const int& interruption_hours)
+{
+    return (double)surgery_points / (interruption_hours + 1);
+}
+
+int predict_power_outage()
+{
+    return
+}
+
+void Outages::add_power_outage_time(const int& t)
+{
+    total_hours_out += t;
+}
+
+void Outages::reset_power_outage_time()
+{
+    total_hours_out = 0;
+}
+
+void Outages::add_customer_hr_interrupt(const int& t)
+{
+    total_customers_hours_interrupted += t;
+}
+
+void Outages::reset_customer_hr_interrupt()
+{
+    total_customers_hours_interrupted = 0;
 }
--- naturaldisasters.h
+++ naturaldisasters.h
@@ -13,5 +13,17 @@ class Outages {
 public:
     Outage* tail;
     void add_outage(Outage* new_outage);
+    double predict_next_year_performance(const int& surgery_points, const int& interruption_hours);
+    int predict_power_outage();
+
+    void add_power_outage_time(const int& t);
+    void reset_power_outage_time();
+
+    void add_customer_hr_interrupt(const int& t);
+    void reset_customer_hr_interrupt();
+
+private:
+    int total_hours_out;
+    int total_customers_hours_interrupted;
     Outages();
 };
